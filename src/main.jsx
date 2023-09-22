5f1fe2fb8bada6d0688f9734e069baea9d5eb891
vhraposo <thevhup@gmail.com>
2023-09-22 18:14:01 -0300

    feat: add new folder 'styles' -> global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details.jsx'
+import { Details } from './pages/Details'
 
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Details />
-  </React.StrictMode>,
+  <React.StrictMode> 
+    <ThemeProvider theme={ theme }>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>  
+  </React.StrictMode>
 )
