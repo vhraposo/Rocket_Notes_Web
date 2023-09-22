ba69c6780fea354de5fcdea1c9d99cfb1553e7ee
vhraposo <thevhup@gmail.com>
2023-09-21 23:04:17 -0300

    refactor: clean project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Details } from './pages/Details.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Details />
   </React.StrictMode>,
 )
