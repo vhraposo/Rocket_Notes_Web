5bcdb952b98ccb5c219317e15d4650c08a1ae632
vhraposo <thevhup@gmail.com>
2023-09-23 14:39:10 -0300

    feat: add buttom component at components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,16 @@
 import { Container } from "./styles"
+import { Button } from '../../components/Button'
 
 export function Details(){
   return(
     <Container>
       <h1>Hello, World!</h1>
       <span>Victor Raposo</span>
+
+     <Button title='Entrar' loading />
+     <Button title='Cadastrar' />
+     <Button title='Voltar' />
+
     </Container>
     
   )
