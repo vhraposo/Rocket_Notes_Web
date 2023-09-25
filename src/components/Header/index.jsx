79825f5539f0efb5616e718e4d67842624372e47
vhraposo <thevhup@gmail.com>
2023-09-25 10:19:46 -0300

    feat: add user image at header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,18 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 
 
 export function Header(){
     return(
         <Container>
-            
+            <Profile>
+                <img src="https://github.com/vhraposo.png" alt="Foto do usuário" />
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Victor Raposo</strong>
+                </div>
+            </Profile>
         </Container>
     )
 }
