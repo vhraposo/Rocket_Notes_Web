f440a5cbe888329bad2881853344ddea128db3bd
vhraposo <thevhup@gmail.com>
2023-09-27 23:33:28 -0300

    feat: create Tag component at components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,8 @@ import { Container, Links } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+
 
 export function Details() {
   return (
@@ -10,15 +12,14 @@ export function Details() {
       <Header />
       <Section title="Links úteis">
         <Links>
-          <li>
-            {' '}
-            <a href="#">https://www.rocketseat.com.br</a>
-          </li>
-          <li>
-            {' '}
-            <a href="#">https://www.rocketseat.com.br</a>
-          </li>
+          <li><a href="#">https://rocketseat.com.br</a></li>
+          <li><a href="#">https://rocketseat.com.br</a></li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+          <Tag title="express"/>
+          <Tag title="node"/>
       </Section>
 
       <Button title="Voltar" />
