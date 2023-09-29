650468d37b683c2e08e1677e7412bdb84550f246
vhraposo <thevhup@gmail.com>
2023-09-29 10:21:06 -0300

    feat: create and styling input component at home screen

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,9 @@
-import { FiPlus } from 'react-icons/fi'
+import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 
 import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
 import { ButtonText } from '../../components/ButtonText'
 
 
@@ -22,7 +23,7 @@ export function Home(){
             </Menu>
 
             <Search>
-
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
             </Search>
 
             <Content>
