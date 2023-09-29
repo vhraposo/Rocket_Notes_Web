1786cf13c890d4cd11ffcb66f4125835ea80de68
vhraposo <thevhup@gmail.com>
2023-09-29 10:02:49 -0300

    refactor: resolving styled components console alerts

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -3,7 +3,7 @@ import { Container } from "./styles";
 
 export function ButtonText({ title, isActive = false, ...rest}){
     return(
-        <Container type="button" isActive={isActive} {...rest}>
+        <Container type="button" $isactive={isActive.toString()} {...rest}>
             {title}
         </Container>
     )
