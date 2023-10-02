10763109f9306e34b1913b87f6df226a15b5d3ef
vhraposo <thevhup@gmail.com>
2023-10-02 09:58:38 -0300

    feat: create signIn page at pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,13 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
-import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100vh;
+
+    display: flex;
+    align-items: stretch;
+
+`
