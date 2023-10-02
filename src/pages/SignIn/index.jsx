c3303381cedaddaa3b2918f8b5e0c1461c87e200
vhraposo <thevhup@gmail.com>
2023-10-02 10:10:51 -0300

    style: styling signIn page at pages

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,24 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
 
 export function SignIn(){
     return(
        <Container>
-            
+           <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis</p>
+
+                <h2>Faça seu login</h2>
+                <Input placeholder="E-mail" type="text" icon={FiMail}/>
+                <Input placeholder="Senha" type="password" icon={FiLock}/>
+
+                <Button title="Entrar" />
+
+                <a href="">Crar conta</a>
+            </Form> 
        </Container> 
     )
 }
