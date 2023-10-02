db469816330c04a6814c978255b612454f2d84ca
vhraposo <thevhup@gmail.com>
2023-10-02 15:05:20 -0300

    style add picture of user at profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,10 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
 
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 
 
@@ -17,6 +17,13 @@ export function Profile(){
             </header>
 
             <Form>
+                <Avatar>
+                    <img src="https://github.com/vhraposo.png" alt="Foto do usuário" />
+                    <label htmlFor="avatar">
+                        <FiCamera/>
+                        <input id='avatar' type='file' />
+                    </label>
+                </Avatar>
                 <Input placeholder="Nome" type="text" icon={FiUser} />
                 <Input placeholder="E-mail" type="text" icon={FiMail} />
                 <Input placeholder="Senha atual" type="password" icon={FiLock} />
