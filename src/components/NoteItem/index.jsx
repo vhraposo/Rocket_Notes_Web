7e4b7f8d49965c9158807b525504fb142f682c6b
vhraposo <thevhup@gmail.com>
2023-10-03 13:53:33 -0300

    feat: create NoteItem at components

---
+++ src/components/NoteItem/index.jsx
@@ -0,0 +1,15 @@
+import { FiPlus, Fix} from 'react-icons/fi'
+
+import { Container } from './styles'
+
+export function NoteItem({ isNew, value, onClick, ...rest}){
+    return(
+        <Container isNew={isNew} >
+            <input type="text" value={value} readOnly={!isNew} {...rest} />
+        
+            <button type='button' onClick={onClick}>
+                {isNew ? <FiPlus /> : <FiX/>}
+            </button>
+        </Container>
+    )
+}
