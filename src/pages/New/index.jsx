b5c5f28d61f089da026f29d901e5f59ad6f1c24a
vhraposo <thevhup@gmail.com>
2023-10-03 13:39:55 -0300

    feat: create new Form note at new note page

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,10 +1,24 @@
 import { Header } from '../../components/Header'
-import { Container } from './styles'
+import { Input } from '../../components/Input'
+
+
+import { Container, Form } from './styles'
 
 export function New(){
     return(
         <Container>
             <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">Voltar</a>
+                    </header>
+
+                    <Input placeholder="Título" />
+                </Form>
+            </main>
         </Container>
     )
 }
