6ae13709554fbd2b2bfd8c07d3d90cc200dae857
vhraposo <thevhup@gmail.com>
2023-10-16 14:33:58 -0300

    feat: set and get local storage token and user

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from '../services/api'
 
 export const AuthContext = createContext({})
@@ -10,8 +10,10 @@ function AuthProvider({ children }){
         
         try{
             const response = await api.post('/sessions', { email, password})
-            
             const { user, token } = response.data
+
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+            localStorage.setItem("@rocketnotes:token", token)
             
             api.defaults.headers.authorization = `Bearer ${token}`
             setData({ user, token })
@@ -26,6 +28,19 @@ function AuthProvider({ children }){
             }
         }
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketnotes:token")
+        const user = localStorage.getItem("@rocketnotes:user")
+        if(token && user ){
+            api.defaults.headers.authorization = `Bearer ${token}`
+            setData({
+                token,
+                user: JSON.parse(user)
+            })
+        }
+    }, [])
+
 
     return (
       <AuthContext.Provider value={ { signIn, user: data.user } }>
