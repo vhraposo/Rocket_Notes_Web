24626328c1677450b160fbd2d27e761740caccad
vhraposo <thevhup@gmail.com>
2023-10-16 10:44:23 -0300

    feat: add SignIn auth at hooks

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Container, Form, Background } from "./styles";
 import { Link } from 'react-router-dom'
 import { FiMail, FiLock } from 'react-icons/fi'
@@ -8,9 +10,15 @@ import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 
 export function SignIn(){
+    const [ email, setEmail] = useState("")
+    const [ password, setPassword] = useState("")
 
-    const data = useAuth()
-    console.log('Meu contexto ->', data)
+    const { signIn } = useAuth()
+
+    function handleSignIn(){
+        signIn({email, password })
+    }
+    
 
     return(
        <Container>
@@ -19,10 +27,10 @@ export function SignIn(){
                 <p>Aplicação para salvar e gerenciar seus links úteis</p>
 
                 <h2>Faça seu login</h2>
-                <Input placeholder="E-mail" type="text" icon={FiMail}/>
-                <Input placeholder="Senha" type="password" icon={FiLock}/>
+                <Input placeholder="E-mail" type="text" icon={FiMail} onChange={ e => setEmail(e.target.value)} />
+                <Input placeholder="Senha" type="password" icon={FiLock} onChange={ e => setPassword(e.target.value)} />
 
-                <Button title="Entrar" />
+                <Button title="Entrar" onClick={handleSignIn} />
 
                 <Link to='/register'>Crar conta</Link>
             </Form> 
