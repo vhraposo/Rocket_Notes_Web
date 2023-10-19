d09590ca9cabcb12ab614ae77f3e80ffffd541e2
vhraposo <thevhup@gmail.com>
2023-10-18 22:09:26 -0300

    feat: add  note remove at new note component

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -30,6 +30,10 @@ export function New(){
         setNewTag("")
     }
 
+    function handleRemoveTag(deleted){
+        setTags(prevState => prevState.filter(tag => tag !== deleted))
+    }
+
     return(
         <Container>
             <Header />
@@ -70,7 +74,7 @@ export function New(){
                                     <NoteItem 
                                     key={String(index)} 
                                     value={tag}
-                                    onClick={() => { }}
+                                    onClick={() => handleRemoveTag(tag)}
                                     />
                                 ))
                             }
