cdd2d039858d1813b0ff968e2e9f99addd65b802
vhraposo <thevhup@gmail.com>
2023-10-21 16:56:07 -0300

    deploy: add baseurl deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333"
+    // baseURL: "http://localhost:3333"
+    baseURL: "https://rocketnotes-api-ufri.onrender.com"
 })
 
