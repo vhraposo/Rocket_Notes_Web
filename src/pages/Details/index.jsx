47c274dcad95cd4acee5a821b02bdbfd145ccdfc
vhraposo <thevhup@gmail.com>
2023-10-21 13:51:30 -0300

    feat: add notes details loading at details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,38 +1,75 @@
+import { useState, useEffect } from 'react'
 import { Container, Links, Content } from './styles'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { ButtonText } from '../../components/ButtonText'
 import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
+import { api } from '../../services/api'
 
 
 export function Details() {
+  const [data, setData] = useState(null)
+
+  const params = useParams()
+  const navigate = useNavigate()
+
+  function handleBack(){
+    navigate('/')
+  }
+
+  useEffect(() => {
+    async function fetchNote(){
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  }, [])
+
   return (
     <Container>
       <Header />
 
-      <main>
+      {
+        data &&
+        <main>
         <Content>
           <ButtonText title="Excluir nota" />
 
-          <h1>Introdução ao React</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, hic? Hic, rem vitae. Quod libero saepe laboriosam dolore odit iusto, tempore obcaecati tempora consectetur quia eaque? Et rem sequi repudiandae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, hic? Hic, rem vitae. Quod libero saepe laboriosam dolore odit iusto, tempore obcaecati tempora consectetur quia eaque? Et rem sequi repudiandae</p>
-          <Section title="Links úteis">
-            <Links>
-              <li><a href="#">https://rocketseat.com.br</a></li>
-              <li><a href="#">https://rocketseat.com.br</a></li>
-            </Links>
-          </Section>
+          <h1>{data.title}</h1>
+          <p>{data.description}</p>
 
-          <Section title="Marcadores">
-              <Tag title="express"/>
-              <Tag title="node"/>
-          </Section>
+          {
+            data.links &&
+            <Section title="Links úteis">
+              <Links>
+                {
+                  data.links.map(link => (
+                    <li key={String(link.id)}><a href={link.url} target='_blank' >{link.url}</a></li>
+                  ))
+                }
+              </Links>
+            </Section>
+          }
 
-          <Button title="Voltar" />
+          {
+            data.tags &&
+            <Section title="Marcadores">
+            {
+              data.tags.map(tag => (
+              <Tag key={String(tag.id)} title={tag.name}/>
+              ))
+            }  
+            </Section>
+          }
+
+          <Button title="Voltar" onClick={handleBack} />
       </Content>
-      </main>
+        </main>
+      }
     </Container>
   )
 }
